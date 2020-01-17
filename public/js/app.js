ac3f2f3923b67514442340256325f4cef49b2eb5
GonzaloEspinoza <gonzalo.espinoza@axity.com>
2020-01-17 16:51:39 -0300

    Se agrega gitiggnore

--- public/js/app.js
+++ public/js/app.js
@@ -15,14 +15,16 @@ weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
 
     const localizacion = search.value;
-    
+    messageOne.textContent = 'Cargando ....';
+    messageTwo.textContent = '';
     fetch('http://localhost:3000/weather?address='+ localizacion).then((response) => {
     response.json().then((data) => {
             if(data.error){
-                console.log(data.error);
+                messageOne.textContent = data.error;
+                messageTwo.textContent = '';
             }else{
-                console.log(data.localizacion);
-                console.log(data.forecast);
+                messageOne.textContent = data.localizacion;
+                messageTwo.textContent = data.forecast;
             }
         })
     })
