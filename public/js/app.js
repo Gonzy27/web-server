e00a41e7783826b531d2f17317bf0939863c4fb5
GonzaloEspinoza <gonzaloespinoza.alarcon@gmail.com>
2020-01-18 22:59:55 -0300

    fix node_modules

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('Client side javascript file is loaded!');
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data);
-    })
-});
-
 const weatherForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
@@ -13,11 +5,11 @@ const messageTwo = document.querySelector('#message-2');
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
+    
     const localizacion = search.value;
     messageOne.textContent = 'Cargando ....';
     messageTwo.textContent = '';
-    fetch('http://localhost:3000/weather?address='+ localizacion).then((response) => {
+    fetch('/weather?address='+ localizacion).then((response) => {
     response.json().then((data) => {
             if(data.error){
                 messageOne.textContent = data.error;
--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ const geocode = require('./utils/geocode');
 const forecast = require('./utils/forecast')
 
 const app = express();
+const port = process.env.PORT || 3000;
 
 // Definir rutas para configuración de Express
 const publicDirectoryPath = path.join(__dirname, '../public');
@@ -103,6 +104,6 @@ app.get('*', (req, res) =>{
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server levantado en el puerto 3000');
+app.listen(port , () => {
+    console.log(`Server levantado en el puerto ${port}`);
 });
